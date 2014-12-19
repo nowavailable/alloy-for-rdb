a740c76b8d170b8f4affaad2105665d7b25344d6
nowavailable <tsutsumitaiga@gmail.com>
2014-12-19 13:52:32 +0900

    Remove unused import.

--- src/main/java/com/testdatadesigner/tdalloy/core/io/ISchemaSplitter.java
+++ src/main/java/com/testdatadesigner/tdalloy/core/io/ISchemaSplitter.java
@@ -1,11 +1,10 @@
 package com.testdatadesigner.tdalloy.core.io;
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
 
 public interface ISchemaSplitter {
-    public void prepare(InputStream ddlAll);
+    public void prepare(InputStream ddlAll) throws IOException;
     public List<String> getRawTables();
 }
--- src/test/java/com/testdatadesigner/tdalloy/core/io/impl/MySQLSchemaSplitterTest.java
+++ src/test/java/com/testdatadesigner/tdalloy/core/io/impl/MySQLSchemaSplitterTest.java
@@ -2,7 +2,6 @@ package com.testdatadesigner.tdalloy.core.io.impl;
 
 import junit.framework.TestCase;
 
-import java.io.File;
 import java.io.InputStream;
 import java.util.List;
 import java.util.regex.Matcher;
