2729308feffc59028fe0a36472706573ad863553
nowavailable <taiga.tsutsumi@gmail.com>
2014-12-21 02:12:30 +0900

    add ImportManager.

---
+++ src/main/java/com/testdatadesigner/tdalloy/core/io/ImportManager.java
@@ -0,0 +1,15 @@
+package com.testdatadesigner.tdalloy.core.io;
+
+import com.testdatadesigner.tdalloy.core.types.Alloyable;
+
+/**
+ * + データスキーマインポートファイルのI/O
+ * + 結果出力ファイルのI/O
+ * + 結果出力前オブジェクトの状態操作
+ */
+public class ImportManager {
+
+	public void loadScheme() {
+		
+	}
+}
