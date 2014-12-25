3904ff42724c067c3e3d9307ba1e2f5df1157b16
nowavailable <taiga.tsutsumi@gmail.com>
2014-12-25 17:31:53 +0900

    add some new classes.

---
+++ src/main/java/com/testdatadesigner/tdalloy/client/types/Parameterized.java
@@ -0,0 +1,25 @@
+package com.testdatadesigner.tdalloy.client.types;
+
+import java.io.Serializable;
+
+public class Parameterized implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    public Parameterized() {
+        // TODO Auto-generated constructor stub
+    }
+    
+    // テーブルリスト
+    
+    // カラムリスト
+
+    // グループ分けされたカラムリスト（マークでもいいか）
+    
+    // 関係リスト
+
+    // テーブル - 参照関係
+    // テーブル - ポリモーフィック関係（多重許容）
+    // カラム - 参照関係
+    // カラム - ポリモーフィック関係
+
+}
--- src/main/java/com/testdatadesigner/tdalloy/core/io/ImportManager.java
+++ src/main/java/com/testdatadesigner/tdalloy/core/io/ImportManager.java
@@ -1,5 +1,7 @@
 package com.testdatadesigner.tdalloy.core.io;
 
+import java.io.InputStream;
+
 /**
  * + データスキーマインポートファイルのI/O + 結果出力ファイルのI/O + 結果出力前オブジェクトの状態操作
  */
@@ -7,5 +9,9 @@ public class ImportManager {
 
     public void loadScheme() {
 
+    }
+    
+    public void prepareToAlloy(InputStream in) {
+        
     }
 }
