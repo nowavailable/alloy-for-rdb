0fc5108c30a4db3b4381f688d5647593e4dfa952
nowavailable <tsutsumitaiga@gmail.com>
2016-02-05 21:35:37 +0900

    Add assertion.

--- src/test/java/com/testdatadesigner/tdalloy/core/types/AlloyableHandlerTest.java
+++ src/test/java/com/testdatadesigner/tdalloy/core/types/AlloyableHandlerTest.java
@@ -16,6 +16,7 @@ import com.testdatadesigner.tdalloy.core.io.impl.MySQLSchemaParser;
 import com.testdatadesigner.tdalloy.core.io.impl.MySQLSchemaSplitter;
 import com.testdatadesigner.tdalloy.igniter.Bootstrap;
 
+import junit.framework.Assert;
 import junit.framework.TestCase;
 
 public class AlloyableHandlerTest extends TestCase {
@@ -103,6 +104,40 @@ public class AlloyableHandlerTest extends TestCase {
         this.currentAlloyable = new Alloyable();
         this.alloyableHandler = new AlloyableHandler(currentAlloyable);
         this.currentAlloyable = this.alloyableHandler.buildFromDDL(this.resultList);
+
+        StringBuilder str = new StringBuilder();
+        try(BufferedReader outputToAlsReader = this.alloyableHandler.outputToAls()){
+            String line = null;
+            while ((line = outputToAlsReader.readLine()) != null) {
+                str.append(line);
+                str.append("\n");
+            }
+        }
+        String expected = new String("open util/boolean\n" + "sig Boundary { val: one Int }\n"
+            + "\n" + "sig Album {\n" + "  \n" + "}\n" + "sig Book {\n" + "  person: lone Person,\n"
+            + "  price: lone Boundary\n" + "}\n" + "sig Paper {\n" + "  person: lone Person,\n"
+            + "  price: lone Boundary\n" + "}\n" + "sig Person {\n" + "  books: set Book,\n"
+            + "  papers: set Paper,\n" + "  zines: set Zine\n" + "}\n" + "sig Photo {\n"
+            + "  photoableType: lone Photo_PhotoableType\n" + "}\n" + "sig Url {\n"
+            + "  urlableType: lone Url_UrlableType\n" + "}\n" + "sig Bookmark {\n" + "  \n" + "}\n"
+            + "sig Zine {\n" + "  person: lone Person,\n" + "  price: lone Boundary,\n"
+            + "  is_old: lone Bool\n" + "}\n" + "abstract sig Photo_PhotoableType {\n" + "  \n"
+            + "}\n" + "sig Dummy1 {\n" + "  photos: set Photo\n" + "}\n" + "sig Dummy2 {\n"
+            + "  photos: set Photo\n" + "}\n" + "sig PhotoableDummy1 {\n"
+            + "  dummy1s: lone Dummy1\n" + "}\n" + "sig PhotoableDummy2 {\n"
+            + "  dummy2s: lone Dummy2\n" + "}\n" + "abstract sig Url_UrlableType {\n" + "  \n"
+            + "}\n" + "sig Dummy3 {\n" + "  urls: set Url\n" + "}\n" + "sig Dummy4 {\n"
+            + "  urls: set Url\n" + "}\n" + "sig UrlableDummy3 {\n" + "  dummy3s: lone Dummy3\n"
+            + "}\n" + "sig UrlableDummy4 {\n" + "  dummy4s: lone Dummy4\n" + "}\n" + "\n"
+            + "fact {\n" + "  Person<:books = ~(Book<:person)\n"
+            + "  Person<:papers = ~(Paper<:person)\n" + "  Person<:zines = ~(Zine<:person)\n"
+            + "  (Photo.photoableType = Photo_PhotoableType) and (all p:Photo | p = (Photo<:photoableType).(p.(Photo<:photoableType)))\n"
+            + "  Dummy1<:photos = ~(Photo<:photoableType.dummy1s)\n"
+            + "  Dummy2<:photos = ~(Photo<:photoableType.dummy2s)\n"
+            + "  (Url.urlableType = Url_UrlableType) and (all u:Url | u = (Url<:urlableType).(u.(Url<:urlableType)))\n"
+            + "  Dummy3<:urls = ~(Url<:urlableType.dummy3s)\n"
+            + "  Dummy4<:urls = ~(Url<:urlableType.dummy4s)\n" + "}\n" + "\n" + "run {}\n");
+        Assert.assertEquals(str.toString(), expected);
     }
 
 
@@ -118,6 +153,28 @@ public class AlloyableHandlerTest extends TestCase {
         this.currentAlloyable = new Alloyable();
         this.alloyableHandler = new AlloyableHandler(currentAlloyable);
         this.currentAlloyable = this.alloyableHandler.buildFromDDL(this.resultList);
+
+        StringBuilder str = new StringBuilder();
+        try(BufferedReader outputToAlsReader = this.alloyableHandler.outputToAls()){
+            String line = null;
+            while ((line = outputToAlsReader.readLine()) != null) {
+                str.append(line);
+                str.append("\n");
+            }
+        }
+        String expected = new String("open util/boolean\n" + "sig Boundary { val: one Int }\n"
+            + "\n" + "sig Actor {\n" + "  charactors: set Charactor,\n" + "  name: one Boundary\n"
+            + "}\n" + "sig Movie {\n" + "  charactors: set Charactor,\n" + "  title: one Boundary\n"
+            + "}\n" + "sig Charactor {\n" + "  actor: lone Actor,\n" + "  movie: lone Movie,\n"
+            + "  goods: set Good,\n" + "  name: one Boundary\n" + "}\n" + "sig Good {\n"
+            + "  charactor: one Charactor,\n" + "  name: one Boundary\n" + "}\n" + "\n" + "fact {\n"
+            + "  Actor<:charactors = ~(Charactor<:actor)\n"
+            + "  Movie<:charactors = ~(Charactor<:movie)\n"
+            + "  Charactor<:goods = ~(Good<:charactor)\n"
+            + "  all e,e':Charactor | e != e' => (e.actor -> e.movie != e'.actor -> e'.movie)\n"
+            + "  all e,e':Good | e != e' => (e.charactor.actor -> e.charactor.movie != e'.charactor.actor -> e'.charactor.movie)\n"
+            + "}\n" + "\n" + "run {}\n");
+        Assert.assertEquals(str.toString(), expected);
     }
 
     public void testBuildAllAndOutputAls_Inconsistency() throws Exception {
@@ -132,6 +189,21 @@ public class AlloyableHandlerTest extends TestCase {
         this.currentAlloyable = new Alloyable();
         this.alloyableHandler = new AlloyableHandler(currentAlloyable);
         this.currentAlloyable = this.alloyableHandler.buildFromDDL(this.resultList);
+
+        StringBuilder str = new StringBuilder();
+        try(BufferedReader outputToAlsReader = this.alloyableHandler.outputToAls()){
+            String line = null;
+            while ((line = outputToAlsReader.readLine()) != null) {
+                str.append(line);
+                str.append("\n");
+            }
+        }
+        String expected = new String("open util/boolean\n" + "sig Boundary { val: one Int }\n"
+            + "\n" + "sig Book {\n" + "  person: lone Boundary,\n" + "  price: lone Boundary\n"
+            + "}\n" + "sig Paper {\n" + "  person: lone Boundary,\n" + "  price: lone Boundary\n"
+            + "}\n" + "sig Photo {\n" + "  photoable_type: lone Boundary\n" + "}\n" + "\n"
+            + "fact {\n" + "}\n" + "\n" + "run {}\n");
+        Assert.assertEquals(str.toString(), expected);
     }
 
     public void testBuildAllAndOutputAls_NoRelations() throws Exception {
@@ -146,5 +218,18 @@ public class AlloyableHandlerTest extends TestCase {
         this.currentAlloyable = new Alloyable();
         this.alloyableHandler = new AlloyableHandler(currentAlloyable);
         this.currentAlloyable = this.alloyableHandler.buildFromDDL(this.resultList);
+
+        StringBuilder str = new StringBuilder();
+        try(BufferedReader outputToAlsReader = this.alloyableHandler.outputToAls()){
+            String line = null;
+            while ((line = outputToAlsReader.readLine()) != null) {
+                str.append(line);
+                str.append("\n");
+            }
+        }
+        String expected = new String("open util/boolean\n" + "sig Boundary { val: one Int }\n" + "\n" + "sig Book {\n"
+            + "  person: lone Boundary,\n" + "  some: lone Boundary,\n" + "  price: lone Boundary\n"
+            + "}\n" + "\n" + "fact {\n" + "}\n" + "\n" + "run {}" + "\n");
+        Assert.assertEquals(str.toString(), expected);
     }
 }
