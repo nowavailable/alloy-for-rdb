29875c2e1372937826b9c30e855a324360fb807c
nowavailable <tsutsumitaiga@gmail.com>
2016-05-01 04:15:02 +0900

    Fix webSocket client to send data to server by using stream.

--- src/main/java/com/testdatadesigner/tdalloy/core/io/EventSocket.java
+++ src/main/java/com/testdatadesigner/tdalloy/core/io/EventSocket.java
@@ -1,5 +1,9 @@
 package com.testdatadesigner.tdalloy.core.io;
 
+import java.io.InputStream;
+import java.io.Reader;
+import java.nio.ByteBuffer;
+
 import javax.websocket.ClientEndpoint;
 import javax.websocket.CloseReason;
 import javax.websocket.OnClose;
@@ -8,6 +12,8 @@ import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
+
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
 
 @ClientEndpoint
 @ServerEndpoint(value="/events/")
@@ -25,6 +31,11 @@ public class EventSocket
         System.out.println("Received TEXT message: " + message);
     }
     
+    @OnMessage
+    public void onTextFrame(ByteBuffer buffer, boolean fin) {
+        System.out.println("Received Stream message: ");
+    }
+    
     @OnClose
     public void onWebSocketClose(CloseReason reason)
     {
@@ -36,4 +47,38 @@ public class EventSocket
     {
         cause.printStackTrace(System.err);
     }
+
+    @OnWebSocketMessage
+    public void onTextMethod(Reader stream) {
+       // TEXT message received, and reported to your socket as a
+       // Reader. (can handle 1 message, regardless of size or 
+       // number of frames)
+        System.out.println("...stream: ");
+    }
+   
+//    @OnWebSocketMessage
+//    public void onTextMethod(WebSocketConnection connection, 
+//                             Reader stream) {
+//       // TEXT message received, and reported to your socket as a
+//       // Reader. (can handle 1 message, regardless of size or 
+//       // number of frames).  Connection that message occurs
+//       // on is reported as well.
+//    }
+   
+    @OnWebSocketMessage
+    public void onBinaryMethod(InputStream stream) {
+       // BINARY message received, and reported to your socket
+       // as a InputStream. (can handle 1 message, regardless
+       // of size or number of frames).
+        System.out.println("...binary stream: ");
+    }
+   
+//    @OnWebSocketMessage
+//    public void onBinaryMethod(WebSocketConnection connection, 
+//                               InputStream stream) {
+//       // BINARY message received, and reported to your socket
+//       // as a InputStream. (can handle 1 message, regardless
+//       // of size or number of frames).  Connection that 
+//       // message occurs on is reported as well.
+//    }
 }
--- src/test/java/com/testdatadesigner/tdalloy/core/io/alloyableJsonWebSocketStream.js
+++ src/test/java/com/testdatadesigner/tdalloy/core/io/alloyableJsonWebSocketStream.js
@@ -3,32 +3,62 @@
  * $ nvm use 5.11.0 
  * $ npm install java  
  * $ npm install websocket-stream
- * $ npm install JSONStream 
  * $ java -jar target/tdalloy-0.1.0-SNAPSHOT-jar-with-dependencies.jar 
  * $ node src/test/java/com/testdatadesigner/tdalloy/core/io/alloyableWebSocketStream.js
  */
-var JSONStream = require('../../../../../../../../node_modules/JSONStream');
+var stream = require('stream')
+//  , util = require('util')
+  ;
+
+//var JSONStream = require('../../../../../../../../node_modules/JSONStream');
 var java = require("../../../../../../../../node_modules/java");
 java.classpath.push("./target/tdalloy-0.1.0-SNAPSHOT-jar-with-dependencies.jar");
 var imp = java.newInstanceSync("com.testdatadesigner.tdalloy.core.io.Importer");
 java.callStaticMethodSync("com.testdatadesigner.tdalloy.igniter.Bootstrap","setProps")
-var alloyable = JSON.parse(java.callMethodSync(imp, "getAlloyableJSON","./src/test/resources/naming_rule.dump", "mysql"));
+//var alloyable = JSON.parse(java.callMethodSync(imp, "getAlloyableJSON","./src/test/resources/naming_rule.dump", "mysql"));
+var alloyable = java.callMethodSync(imp, "getAlloyableJSON","./src/test/resources/naming_rule.dump", "mysql")
 
 var WebSocket = require('../../../../../../../../node_modules/websocket-stream');
-var ws = new WebSocket('ws://localhost:8080/events/');
 
-ws.on('pipe', function () {
-  console.log("Piping");
-  ws.write()
-  //
-  ws.end();
+var fs = require('fs'),
+    path = require('path');
+var filePathOut = path.join(__dirname, "test_out.txt");
+var filePathIn = path.join(__dirname, "test_in.txt");
+
+//var ws = fs.createWriteStream(filePathOut);
+var ws = new WebSocket('ws://localhost:8080/events/');
+//var rs = fs.createReadStream(filePathIn);
+//var rs = JSONStream.parse(alloyable)
+var rs = new stream.Readable({ objectMode: true });
+
+rs.on('data', function(data){
+  console.log("Read ok.");
+  console.log(data.length)
+//  if (ws.write(data) === false) {
+//	  console.log("Read paused.")
+//	  rs.pause()
+//  }
+}).on('end', function(){
+  console.log('Read ended.')
+}).on('close', function(){
+  console.log('Read closed.')
 }).on('error', function(err) {
-  console.log(err);
+  console.log("Read error: " + err);
+  ws.end();
+})
+
+ws.on('pipe', function() {
+  console.log("Piping");
+}).on('error', function(err) {
+  console.log("Write error: " + err);
   ws.end();
   ws.socket.terminate();
 }).on('close', function(err) {
   console.log("Closing");
   ws.socket.terminate();
 });
-var rs = JSONStream.parse(alloyable)
+
+rs.push(alloyable)
+rs.push(null)
 rs.pipe(ws)
+//ws.end();
